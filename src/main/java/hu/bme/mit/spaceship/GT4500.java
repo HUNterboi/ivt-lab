fd0a3d5766eda93be4142ecbe2306d7a2a1739d2
HUNterboi <bogyos.tamas2@gmail.com>
2022-05-06 05:35:26 +0200

    Fix not fully implemented

--- src/main/java/hu/bme/mit/spaceship/GT4500.java
+++ src/main/java/hu/bme/mit/spaceship/GT4500.java
@@ -15,6 +15,11 @@ public class GT4500 implements SpaceShip {
     this.secondaryTorpedoStore = new TorpedoStore(10);
   }
 
+  public GT4500(TorpedoStore primary, TorpedoStore secondary) {
+      this.primaryTorpedoStore = primary;
+      this.secondaryTorpedoStore = secondary;
+  } 
+
   public boolean fireLaser(FiringMode firingMode) {
     // TODO not implemented yet
     return false;
@@ -79,7 +84,15 @@ public class GT4500 implements SpaceShip {
       case ALL:
         // try to fire both of the torpedo stores
         //TODO implement feature
-
+        boolean primarySucces = false; 
+        boolean secondarySucces = false; 
+        if(primaryTorpedoStore.isEmpty() && secondaryTorpedoStore.isEmpty()) {
+          firingSuccess = false; 
+        }
+        else {
+          primaryTorpedoStore.fire(primaryTorpedoStore.getTorpedoCount());
+          firingSuccess = secondaryTorpedoStore.fire(secondaryTorpedoStore.getTorpedoCount());  
+        }  
         break;
     }
 
--- src/test/java/hu/bme/mit/spaceship/GT4500Test.java
+++ src/test/java/hu/bme/mit/spaceship/GT4500Test.java
@@ -9,31 +9,42 @@ import static org.mockito.Mockito.*;
 public class GT4500Test {
 
   private GT4500 ship;
+  private TorpedoStore primary;
+  private TorpedoStore secondary;  
 
   @BeforeEach
   public void init(){
-    this.ship = new GT4500();
+    primary = mock(TorpedoStore.class); 
+    secondary = mock(TorpedoStore.class); 
+    this.ship = new GT4500(primary, secondary);
   }
 
   @Test
   public void fireTorpedo_Single_Success(){
     // Arrange
+    when(primary.fire(1)).thenReturn(true);
 
     // Act
     boolean result = ship.fireTorpedo(FiringMode.SINGLE);
 
     // Assert
+    verify(primary, times(1)).fire(1); 
     assertEquals(true, result);
   }
 
   @Test
   public void fireTorpedo_All_Success(){
     // Arrange
+    when(primary.fire(primary.getTorpedoCount())).thenReturn(true);
+    when(secondary.fire(primary.getTorpedoCount())).thenReturn(true); 
+
 
     // Act
     boolean result = ship.fireTorpedo(FiringMode.ALL);
 
     // Assert
+    verify(primary, times(1)).fire(primary.getTorpedoCount()); 
+    verify(secondary, times(1)).fire(secondary.getTorpedoCount()); 
     assertEquals(true, result);
   }
 
